070adcd88e98d8b42856091ade68727b52c0343a
Dor Kleiman <configurator@gmail.com>
2017-11-29 21:15:14 +0200

    removed input (don't worry, we'll be back)

--- snake/src/scripts/input.js
+++ snake/src/scripts/input.js
@@ -1,28 +1,28 @@
-const process = require('process');
-require('keypress')(process.stdin);
+// const process = require('process');
+// require('keypress')(process.stdin);
 
-const ansi = require('ansi');
-const cursor = ansi(process.stdout);
+// const ansi = require('ansi');
+// const cursor = ansi(process.stdout);
 
-const { up, down, left, right } = require('./constants');
-const gameState = require('./gamestate');
+// const { up, down, left, right } = require('./constants');
+// const gameState = require('./gamestate');
 
-process.stdin.setRawMode(true);
-process.stdin.on('keypress', function (chunk, event) {
-	switch (event.name){
-		case 'q':
-		case 'escape':
-			cursor.show();
-			process.exit();
-			break;
+// process.stdin.setRawMode(true);
+// process.stdin.on('keypress', function (chunk, event) {
+// 	switch (event.name){
+// 		case 'q':
+// 		case 'escape':
+// 			cursor.show();
+// 			process.exit();
+// 			break;
 
-		case up:
-		case down:
-		case left:
-		case right:
-			gameState.direction = event.name;
-			break;
-	}
-});
+// 		case up:
+// 		case down:
+// 		case left:
+// 		case right:
+// 			gameState.direction = event.name;
+// 			break;
+// 	}
+// });
 
-setInterval(function() {}, 10000);
+// setInterval(function() {}, 10000);
