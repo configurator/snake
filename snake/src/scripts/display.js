370e35956fc689aae23cb4dabb56d29a9bb25c7b
Dor Kleiman <configurator@gmail.com>
2017-11-29 21:14:50 +0200

    rewrite display for web

--- snake/src/scripts/display.js
+++ snake/src/scripts/display.js
@@ -1,68 +1,97 @@
-const ansi = require('ansi');
-const cursor = ansi(process.stdout);
-
 const gameState = require('./gamestate');
-const { isIn } = require('./util');
 
 function print() {
-	cursor.hide();
+	var board = document.querySelector('.game-board');
+	board.innerHTML = '';
+
+	gameState.snake.forEach(function (part) {
+		var snakeElement = document.createElement('i');
+		snakeElement.classList.add("snake");
+		snakeElement.style.top = part.y + "em";
+		snakeElement.style.left = part.x + "em";
+		board.appendChild(snakeElement);
+	});
+
+	gameState.apples.forEach(function (apple){
+		var appleElement = document.createElement('i');
+		appleElement.classList.add("apple");
+		appleElement.style.top = apple.y + "em";
+		appleElement.style.left = apple.x + "em";
+		board.appendChild(appleElement);
+	});
 
 	if (gameState.dead){
-		if(!gameState.deadPrinted) {
-			drawDead();
-			gameState.deadPrinted = true;
-		}
-		return;
-	}
-
-	cursor.goto(2,2);
-
-	for (var y = 0; y < gameState.boardSize; y++) {
-		for (var x = 0; x < gameState.boardSize; x++) {
-			cursor.goto( x+2, y+2)
-			if (isIn(x, y, gameState.snake)) {
-				 cursor.write('█');
-			} else if (isIn(x, y, gameState.apples)) {
-				cursor.write('☭');
-			} else {
-				cursor.write(' ');
-			}
-		}
-		//cursor.write('\n'); //on work computer I had to cancel this line to stop game board from sliding down
+		document.body.classList.add("youlose");
 	}
 }
 
-function drawboard(space) {
-	cursor.goto(1,1);
-	cursor.eraseData();
+function drawboard() {}
 
-	cursor.write('┌');
-	for (var x = 0; x < gameState.boardSize; x++) {
-		cursor.write('─');
-	}
-	cursor.write('┐');
-	cursor.write('\n');
-	for (var y = 0; y < gameState.boardSize; y++) {
-		cursor.goto(1, y + 2);
-		cursor.write('│');
-		for (var x = 0; x < gameState.boardSize; x++) {
-			cursor.write(space);
-		}
-		//cursor.goto(gameState.boardSize, y);
-		cursor.write('│');
-		cursor.write('\n');
-	}
-	cursor.write('└');
-	for (var x = 0; x < gameState.boardSize; x++) {
-		cursor.write('─');
-	}
-	cursor.write('┘');
-	cursor.write('\n');
-}
+// const ansi = require('ansi');
+// const cursor = ansi(process.stdout);
 
-function drawDead() {
-	drawboard('♿');
-}
+//
+// const { isIn } = require('./util');
+
+// function print() {
+// 	cursor.hide();
+
+// 	if (gameState.dead){
+// 		if(!gameState.deadPrinted) {
+// 			drawDead();
+// 			gameState.deadPrinted = true;
+// 		}
+// 		return;
+// 	}
+
+// 	cursor.goto(2,2);
+
+// 	for (var y = 0; y < gameState.boardSize; y++) {
+// 		for (var x = 0; x < gameState.boardSize; x++) {
+// 			cursor.goto( x+2, y+2)
+// 			if (isIn(x, y, gameState.snake)) {
+// 				 cursor.write('█');
+// 			} else if (isIn(x, y, gameState.apples)) {
+// 				cursor.write('☭');
+// 			} else {
+// 				cursor.write(' ');
+// 			}
+// 		}
+// 		//cursor.write('\n'); //on work computer I had to cancel this line to stop game board from sliding down
+// 	}
+// }
+
+// function drawboard(space) {
+// 	cursor.goto(1,1);
+// 	cursor.eraseData();
+
+// 	cursor.write('┌');
+// 	for (var x = 0; x < gameState.boardSize; x++) {
+// 		cursor.write('─');
+// 	}
+// 	cursor.write('┐');
+// 	cursor.write('\n');
+// 	for (var y = 0; y < gameState.boardSize; y++) {
+// 		cursor.goto(1, y + 2);
+// 		cursor.write('│');
+// 		for (var x = 0; x < gameState.boardSize; x++) {
+// 			cursor.write(space);
+// 		}
+// 		//cursor.goto(gameState.boardSize, y);
+// 		cursor.write('│');
+// 		cursor.write('\n');
+// 	}
+// 	cursor.write('└');
+// 	for (var x = 0; x < gameState.boardSize; x++) {
+// 		cursor.write('─');
+// 	}
+// 	cursor.write('┘');
+// 	cursor.write('\n');
+// }
+
+// function drawDead() {
+// 	drawboard('♿');
+// }
 
 module.exports = {
 	print, drawboard
