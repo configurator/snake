5f331a31b4494f06fb03ff1bee08b35b6f7a79f9
Dor Kleiman <configurator@gmail.com>
2017-12-05 21:11:29 +0200

    Add basic input (arrows)

--- snake/src/scripts/input.js
+++ snake/src/scripts/input.js
@@ -1,28 +1,19 @@
-// const process = require('process');
-// require('keypress')(process.stdin);
+const { up, down, left, right } = require('./constants');
+const gameState = require('./gamestate');
 
-// const ansi = require('ansi');
-// const cursor = ansi(process.stdout);
-
-// const { up, down, left, right } = require('./constants');
-// const gameState = require('./gamestate');
-
-// process.stdin.setRawMode(true);
-// process.stdin.on('keypress', function (chunk, event) {
-// 	switch (event.name){
-// 		case 'q':
-// 		case 'escape':
-// 			cursor.show();
-// 			process.exit();
-// 			break;
-
-// 		case up:
-// 		case down:
-// 		case left:
-// 		case right:
-// 			gameState.direction = event.name;
-// 			break;
-// 	}
-// });
-
-// setInterval(function() {}, 10000);
+document.body.addEventListener('keydown', function(event){
+	switch (event.key) {
+		case 'ArrowUp':
+			gameState.direction = up;
+			break;
+		case 'ArrowRight':
+			gameState.direction = right;
+			break;
+		case 'ArrowDown':
+			gameState.direction = down;
+			break;
+		case 'ArrowLeft':
+			gameState.direction = left;
+			break;
+	}
+});
