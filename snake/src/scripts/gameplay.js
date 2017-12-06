7509f7dffd52a2c53f48ec92983c9f1f9f2bdbab
Elad Aaron <31505398+ezrubaal@users.noreply.github.com>
2017-12-06 15:51:03 +0200

    Fix turnCounter to decrease instead of increase and not below 100 ms

--- snake/src/scripts/gameplay.js
+++ snake/src/scripts/gameplay.js
@@ -37,8 +37,8 @@ function tick() {
 		addApple();
 	}
 
-	if (gameState.turnCounter % 50 === 0) {
-		gameState.miliSeconds += 100;	
+	if (gameState.turnCounter % 50 === 0 && gameState.miliSeconds > 100) {
+		gameState.miliSeconds -= 100;	
 	}
 
 	// Calculate new head's location
